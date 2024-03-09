6601e7d5e1c1c386647abe3b000d4fbb1dcab5b3
Marta Villalba <marta.villalba.orta@gmail.com>
2024-03-09 10:49:08 +0100

    refactored of method and validation

--- ex.php
+++ ex.php
@@ -1,34 +1,26 @@
 <?php
+
 $numero = 546;
 $cifra = 1;
-
-$lista_numeros = [453, 1443, 377234];
-$lista_cifras = [3, 7, 7];
-
-//me ha faltado adaptar lo que funciona a un array con varios numeros y cifras
 
 
 function existe_cifra($numero, $cifra)
 {
-    $validacion = false;
-
     while ($numero > 0) {
-        $unidad = $numero % 10; 
+        $unidad = $numero % 10;
         if ($unidad == $cifra) {
-            $validacion = true;
+            return true; 
         }
-        $numero /= 10; 
+        $numero = (int)($numero / 10);
     }
 
-    return $validacion;
+    return false; 
 }
 
 $validacion = existe_cifra($numero, $cifra);
 
-
-if ($validacion == true) {
+if ($validacion) {
     echo "La cifra $cifra está dentro del número $numero";
 } else {
     echo "La cifra $cifra no está dentro del número $numero";
 }
-?>
